3ce3c00de65a166dd114185c97dcf772daf820c0
alejandro_diaz <diazbrandonalejandro@gmail.com>
2022-06-06 18:37:34 +0200

    iteraciones obligatorias Tests OK

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,60 @@
 // Soldier
-class Soldier {}
+class Soldier {
+  constructor(health, strength) {
+    this.health = health
+    this.strength = strength
+  }
+  attack(){
+    return this.strength
+  }
+  receiveDamage(num){
+    this.health -= num
+  }
+}
+
+
 
 // Viking
-class Viking {}
+class Viking extends Soldier{
+  constructor (namePNJ, health, strength,){
+    super(health, strength);
+    this.name = namePNJ;
+  }
+  receiveDamage(num){
+    this.health -= num;
+    if(this.health > 0){
+      return `${this.name} has received ${num} points of damage`
+    }else{ 
+      return `${this.name} has died in act of combat`
+    }
+  }
+  battleCry(){
+    return "Odin Owns You All!"
+  }
+}
+
+//  class Square extends Rectangle {
+//    constructor(widthParam, zHeihthParam) {
+//      construc local
+//      super(widthParam, widthParam); //constriuctor padre.
+//      this.z = zHeihthParam;
+//    }
+//  }
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier{
+  constructor (health, strength){
+    super(health, strength);
+  }  
+  receiveDamage(num){
+    this.health -= num;
+    if(this.health > 0){
+      return `A Saxon has received ${num} points of damage`
+    }else{ 
+      return `A Saxon has died in combat`
+    }
+  }
+}
 
 // War
 class War {}
